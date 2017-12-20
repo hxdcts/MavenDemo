908ef04406f4f2a8ca1dae1dacc41852e3023dd7
Cts <caotieshuan1986@163.com>
2017-12-20 19:02:59 +0800

    Test

---
+++ pfb/src/main/java/com/loan/pfb/App.java
@@ -0,0 +1,13 @@
+package com.loan.pfb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
